ccb2f67aee7f7815b65b1ca4d2bdaedbbfb519df
rafalh26 <rafalh26@gmail.com>
2021-01-05 09:58:53 +0100

    refactor

--- DynamicRectangles/DynamicRectangles/main.cpp
+++ DynamicRectangles/DynamicRectangles/main.cpp
@@ -1,12 +1,16 @@
 #include "Rectangle.h"
 #include <iostream>
+using namespace std;
 
 int main()
 {
 	Rectangle* table[3];
-	Rectangle rect1(20,11);
-	table[0] = new Rectangle(rect1);
-	rect1.printData();
+	table[0] = new Rectangle(200,30);
+	table[1] = new Rectangle(23, 12);
+	table[2] = new Rectangle(12, 8);
+	table[0]->printData();
+	table[1]->printData();
+	table[2]->printData();
 	for (int i = 0; i < 3; i++)
 	{
 		delete table[i];
