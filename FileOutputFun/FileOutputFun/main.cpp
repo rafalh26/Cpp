fd39d8a581063a7b76f80ba6949c48f849ae0621
rafalh26 <rafalh26@gmail.com>
2021-01-07 10:29:33 +0100

    publish

---
+++ FileOutputFun/FileOutputFun/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+void printFormatted(ofstream& outfile, int n);
+
+int main()
+{
+	ofstream outfile;
+
+	int n = 0;
+	cout << "how many numbers?: \n";
+	cin >> n;
+	printFormatted(outfile, n);
+}
+
+void printFormatted(ofstream& outfile, int n)
+{
+	outfile.open("output.txt");
+
+	outfile << fixed << showpoint;
+	cout << fixed << showpoint;
+
+	for (int i = 1; i <= n; i++)
+	{
+		outfile << setw(12) << setprecision(2) << (i * 3.44);
+		outfile << setw(22) << setprecision(10) << (i * 3.44) << endl;
+		cout << setw(12) << setprecision(2) << (i * 3.44);
+		cout << setw(22) << setprecision(10) << (i * 3.44) << endl;
+
+	}
+	outfile.close();
+}
