f2aa6f9dd1f8bac2556f419a9eb5ab4f5961cc16
rafalh26 <rafalh26@gmail.com>
2021-01-18 10:44:17 +0100

    player header implementation

--- RPGProject/RPGProject/Player.h
+++ RPGProject/RPGProject/Player.h
@@ -1,12 +1,32 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <string>
 
 class Player
 {
 public:
+	//constr
+	Player(string name, RACE race, int hitPoints, int magicPoints);
+	
+	//setters
+	void setName(string name);
+	void setRace(RACE race);
+	void setHP(int hitPoints);
+	void setMP(int magicPoints);
 
+	//getters
+	string getname() const;
+	RACE getRace() const;
+	string whatRace() const;
+	int getHP() const;
+	int getMP() const;
+	
+	//abstraction class method
+	void virtual Atack() const = 0;
 private:
-
+	RACE race;
+	string name;
+	int hitPoints, magicPoints;
 };
 
 
